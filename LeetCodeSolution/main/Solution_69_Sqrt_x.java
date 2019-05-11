ed6e5af975252e56f2216872015ec3a4a093dd3a
superBear007 <yanchaowhu@126.com>
2019-05-11 09:56:09 +0800

    第69题解答

---
+++ LeetCodeSolution/main/Solution_69_Sqrt_x.java
@@ -0,0 +1,9 @@
+public class Solution_69_Sqrt_x {
+    public int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+    // 最终提交时间为20190511
+    // 运行时间1ms，超过100%的用户
+    // 运行内存32.7MB, 超过100%的用户
+    // PS: 偷了个懒，哈哈，以后再用二分法
+}
